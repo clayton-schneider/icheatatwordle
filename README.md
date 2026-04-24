ae68f1150a865102265937d91d31f0d50cf72bc9
Clayton <schneiderc2016@gmail.com>
2026-04-24 13:17:50 -0400

    start over for vanilla

--- README.md
+++
@@ -1,3 +0,0 @@
-# I Cheat At Wordle
-
-This is an app to help you win [Wordle](https://icheatatwordle.com).
--- src/components/PageHeader.tsx
+++
@@ -1,9 +0,0 @@
-export default function PageHeader() {
-  return (
-    <header className="px-edge py-5">
-      <div className="text-center">
-        <h1 className="text-lg font-bold">I Cheat At Wordle</h1>
-      </div>
-    </header>
-  );
-}
